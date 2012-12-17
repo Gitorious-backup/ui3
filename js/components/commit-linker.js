6b8dcaa8451f5c2b554535166322669a21e61657
Christian Johansen <christian@cjohansen.no>
2012-12-17 13:37:58 +0100

    Add docs

--- js/components/commit-linker.js
+++ js/components/commit-linker.js
@@ -2,6 +2,16 @@
 var gts = this.gts || {};
 
 /**
+ * Adds a live event handler to a root element that fires on any
+ * element with the class "gts-commit-oid". This is used to link
+ * commit oids from Dolt to commit pages in Gitorious.
+ *
+ * root - The root element where "gts-commit-oid" links are expected
+ * urlTemplate - The URL template to link to. Should be a URL with the
+ *               #{oid} placeholder.
+ * handler - The function to call when a commit link is clicked. The
+ *           handler will be called with the resolved URL as its only
+ *           argument.
  */
 gts.commitLinker = function (root, urlTemplate, handler) {
     var selector = ".gts-commit-oid";
