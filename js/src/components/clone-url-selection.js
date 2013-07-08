48d8ac715d60dcd6e0d656303fb5fc4b63f781d6
Christian Johansen <christian@cjohansen.no>
2013-07-08 12:39:07 +0200

    Properly copy ssh URLs (don't let browser resolve)

--- js/src/components/clone-url-selection.js
+++ js/src/components/clone-url-selection.js
@@ -4,22 +4,32 @@ this.gts = this.gts || {};
 /**
  * When clicking clone URLs, copy them to the related input field.
  */
-this.gts.cloneUrlSelection = function cloneUrlSelection(element) {
-    var input = element.getElementsByTagName("input")[0];
-
-    if (input) {
-        dome.on(input, "focus", function (e) { this.select(); });
-    }
-
-    dome.delegate.bycn("gts-repo-url", element, "click", function (e) {
-        e.preventDefault();
+this.gts.cloneUrlSelection = (function () {
+    function selectCloneUrl(element, selected) {
         var links = dome.byClass("gts-repo-url", element);
         cull.doall(cull.partial(dome.cn.rm, "active"), links);
-        if (e.target) {
-            dome.cn.add("active", e.target);
-            var input = dome.byClass("gts-current-repo-url")[0];
-            input.value = e.target.href;
+        if (selected) {
+            dome.cn.add("active", selected);
+            var input = dome.byClass("gts-current-repo-url", element)[0];
+            input.value = selected.getAttribute("href").replace("ssh://", "");
             input.focus();
         }
-    });
-};
+    }
+
+    function cloneUrlSelection(element) {
+        var input = element.getElementsByTagName("input")[0];
+
+        if (input) {
+            dome.on(input, "focus", function (e) { this.select(); });
+        }
+
+        dome.delegate.bycn("gts-repo-url", element, "click", function (e) {
+            e.preventDefault();
+            e.stopPropagation();
+            selectCloneUrl(element, e.target);
+        });
+    }
+
+    cloneUrlSelection.select = selectCloneUrl;
+    return cloneUrlSelection;
+}());
--- js/test/components/clone-url-selection-test.js
+++ js/test/components/clone-url-selection-test.js
@@ -5,21 +5,21 @@ buster.testCase("Clone URL selection", {
         /*:DOC element = <div class="btn-group gts-repo-urls">
             <a class="btn gts-repo-url" href="git://gitorious.org/gitorious/mainline.git">Git</a>
             <a class="btn gts-repo-url" href="http://git.gitorious.org/gitorious/mainline.git">HTTP</a>
-            <a class="active btn gts-repo-url" href="git@gitorious.org:gitorious/mainline.git">SSH</a>
+            <a class="active btn gts-repo-url" href="ssh://git@gitorious.org:gitorious/mainline.git">SSH</a>
             <input class="span4 gts-current-repo-url gts-select-onfocus" type="url" value="git@gitorious.org:gitorious/mainline.git">
             <button data-toggle="collapse" data-target="#repo-url-help" class="gts-repo-url-help btn">?</button>
           </div>*/
         this.input = this.element.getElementsByTagName("input")[0];
-        document.body.appendChild(this.element);
-        var buttons = this.element.getElementsByTagName("a");
-        this.click = function (num) {
-            jQuery(buttons[num]).trigger("click");
-        };
+        this.buttons = this.element.getElementsByTagName("a");
     },
 
-    "// copies href to input on click": "Click triggering currently not working"
-    /*function () {
-        this.click(0);
+    "copies git:// href to input": function () {
+        gts.cloneUrlSelection.select(this.element, this.buttons[0]);
         assert.equals(this.input.value, "git://gitorious.org/gitorious/mainline.git");
-    }*/
+    },
+
+    "copies ssh:// href to input": function () {
+        gts.cloneUrlSelection.select(this.element, this.buttons[2]);
+        assert.equals(this.input.value, "git@gitorious.org:gitorious/mainline.git");
+    }
 });
