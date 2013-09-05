09e6cd0e1a10c1e35dd425cb10fde1c975ed8cbd
Piotr Solnica <piotr.solnica@gmail.com>
2013-09-05 19:55:19 +0200

    Add support for confirm to rails-links

--- js/src/components/rails-links.js
+++ js/src/components/rails-links.js
@@ -2,23 +2,27 @@
 this.gts = this.gts || {};
 
 this.gts.railsLinks = function (csrfParam, csrfToken) {
-    dome.on(document.body, "click", function (e) {
-        var method = dome.data.get("method", e.target);
-        if (!method || method === "get") { return true; }
-        e.preventDefault();
-        e.stopPropagation();
-        var href = e.target.href;
+  dome.on(document.body, "click", function (e) {
+    var method = dome.data.get("method", e.target);
+    if (!method || method === "get") { return true; }
+    e.preventDefault();
+    e.stopPropagation();
+    var href = e.target.href;
 
-        var form = dome.el("form", {
-            method: "post",
-            action: href,
-            style: { display: "none" }
-        }, [
-            dome.el("input", { name: "_method", value: method, type: "hidden" }),
-            dome.el("input", { name: csrfParam, value: csrfToken, type: "hidden" })
-        ]);
+    var form = dome.el("form", {
+      method: "post",
+      action: href,
+      style: { display: "none" }
+    }, [
+      dome.el("input", { name: "_method", value: method, type: "hidden" }),
+      dome.el("input", { name: csrfParam, value: csrfToken, type: "hidden" })
+    ]);
 
-        document.body.appendChild(form);
-        form.submit();
-    });
+    var confirmMsg = dome.data.get("confirm", e.target);
+
+    if (!confirmMsg || confirm(confirmMsg)) {
+      document.body.appendChild(form);
+      form.submit();
+    }
+  });
 };
