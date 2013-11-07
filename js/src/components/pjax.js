496c1607449569dc8d7e5c56b98bcd26e408cc2c
Piotr Solnica <piotr.solnica@gmail.com>
2013-11-07 15:19:51 +0100

    Fix pjax link handling

--- js/src/components/pjax.js
+++ js/src/components/pjax.js
@@ -1,36 +1,38 @@
 this.gts = this.gts || {};
 
 this.gts.pjax = (function () {
-    var initialized, activeLink;
+  var initialized, activeLink;
 
-    return function (controller, container) {
-        var $controller = jQuery(controller);
+  return function (controller, container) {
+    var $controller = jQuery(controller);
 
-        if (!initialized) {
-            $(container).on("pjax:end", function () {
-                $(activeLink).addClass("active");
-            });
-            initialized = true;
-        }
+    if (!initialized) {
+      $(container).on("pjax:end", function () {
+        $(activeLink).addClass("active");
+      });
+      initialized = true;
+    }
 
-        $controller.pjax("a", container, {
-            fragment: "#gts-pjax-container",
-            timeout: 1000
-        });
+    $controller.pjax("a", container, {
+      fragment: "#gts-pjax-container",
+      timeout: 1000
+    });
 
-        $controller.on("click", "a", function () {
-            // Tab bars have links inside li-elements in lists. If either li element
-            // has the "active" class name, then assume a tab bar, and move the
-            // active class over to the newly clicked link
-            var lis = $(this.parentNode.parentNode.childNodes);
+    $controller.on("click", "a", function (e) {
+      e.preventDefault();
 
-            if (!lis.hasClass("active")) {
-                activeLink = null;
-                return;
-            }
+      // Tab bars have links inside li-elements in lists. If either li element
+      // has the "active" class name, then assume a tab bar, and move the
+      // active class over to the newly clicked link
+      var lis = $(this.parentNode.parentNode.childNodes);
 
-            lis.removeClass("active");
-            activeLink = this.parentNode;
-        });
-    };
+      if (!lis.hasClass("active")) {
+        activeLink = null;
+        return;
+      }
+
+      lis.removeClass("active");
+      activeLink = this.parentNode;
+    });
+  };
 }());
